405fb8a73872d7b067d9274dcd64baf649af7cae
emagweb <emagcodeshake@gmail.com>
2024-12-23 13:44:13 +0200

    hello

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import HelloWorld from 'src/HelloWorld';
+import React from 'react';
+
+const HelloWorld = ({ children, ...props }) => {
+  return <h1 {...props}>{children}</h1>;
+};
+export { HelloWorld };
